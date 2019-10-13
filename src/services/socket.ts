bea333916d8039f84bf44f332457e05c1d5ed335
LeonardsonCC <leonardson.carvalho@gmail.com>
2019-10-13 13:21:00 -0300

    tests for netlify

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,5 +1,5 @@
 import socketIoClient from 'socket.io-client';
 
-const host = '127.0.0.1:3000';
+const host = 'leonardson.netlify.com:3000';
 
 export default socketIoClient(host) as SocketIOClient.Socket;
